02981cbee4285df235c7b02a121bea2a2eb46afb
Blanditto <78723668+Marta-D4@users.noreply.github.com>
2026-01-12 11:14:00 +0100

    Correction

--- ex5/ft_count_harvest_iterative.py
+++ ex5/ft_count_harvest_iterative.py
@@ -1,4 +1,7 @@
 def ft_count_harvest_iterative(days):
+    print(f"Days until harvest: {days}")
     for day in range(1, days + 1):
         print(f"Day {day}")
     print("Harvest time!")
+
+# ft_count_harvest_iterative(5)
